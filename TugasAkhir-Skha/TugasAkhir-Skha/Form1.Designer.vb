1bb63a84afe71d72e2c21ea4c344a0e3cb26e482
skha17 <skhavahlevi@gmail.com>
2025-01-06 20:08:13 +0700

    Add files via upload

---
+++ TugasAkhir-Skha/TugasAkhir-Skha/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        Label1 = New Label()
+        lblNama = New Label()
+        lblNoHP = New Label()
+        txtNama = New TextBox()
+        txtNoHP = New TextBox()
+        lblTujuan = New Label()
+        cmbTujuan = New ComboBox()
+        lblHarga = New Label()
+        txtHarga = New TextBox()
+        lblJumlah = New Label()
+        txtJumlah = New TextBox()
+        lblTotalHarga = New Label()
+        txtTotalHarga = New TextBox()
+        btnPesan = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(btnPesan)
+        Panel1.Controls.Add(txtTotalHarga)
+        Panel1.Controls.Add(lblTotalHarga)
+        Panel1.Controls.Add(txtJumlah)
+        Panel1.Controls.Add(lblJumlah)
+        Panel1.Controls.Add(txtHarga)
+        Panel1.Controls.Add(lblHarga)
+        Panel1.Controls.Add(cmbTujuan)
+        Panel1.Controls.Add(lblTujuan)
+        Panel1.Controls.Add(txtNoHP)
+        Panel1.Controls.Add(txtNama)
+        Panel1.Controls.Add(lblNoHP)
+        Panel1.Controls.Add(lblNama)
+        Panel1.Controls.Add(Label1)
+        Panel1.Location = New Point(38, 177)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(497, 530)
+        Panel1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 11F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(103, 15)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(315, 30)
+        Label1.TabIndex = 0
+        Label1.Text = "Pemesanan Tiket Pariwisata"
+        ' 
+        ' lblNama
+        ' 
+        lblNama.AutoSize = True
+        lblNama.Location = New Point(44, 92)
+        lblNama.Name = "lblNama"
+        lblNama.Size = New Size(59, 25)
+        lblNama.TabIndex = 1
+        lblNama.Text = "Nama"
+        ' 
+        ' lblNoHP
+        ' 
+        lblNoHP.AutoSize = True
+        lblNoHP.Location = New Point(41, 145)
+        lblNoHP.Name = "lblNoHP"
+        lblNoHP.Size = New Size(135, 25)
+        lblNoHP.TabIndex = 2
+        lblNoHP.Text = "No Handphone"
+        ' 
+        ' txtNama
+        ' 
+        txtNama.Location = New Point(182, 89)
+        txtNama.Name = "txtNama"
+        txtNama.Size = New Size(287, 31)
+        txtNama.TabIndex = 3
+        ' 
+        ' txtNoHP
+        ' 
+        txtNoHP.Location = New Point(182, 142)
+        txtNoHP.Name = "txtNoHP"
+        txtNoHP.Size = New Size(287, 31)
+        txtNoHP.TabIndex = 4
+        ' 
+        ' lblTujuan
+        ' 
+        lblTujuan.AutoSize = True
+        lblTujuan.Location = New Point(44, 207)
+        lblTujuan.Name = "lblTujuan"
+        lblTujuan.Size = New Size(64, 25)
+        lblTujuan.TabIndex = 5
+        lblTujuan.Text = "Tujuan"
+        ' 
+        ' cmbTujuan
+        ' 
+        cmbTujuan.FormattingEnabled = True
+        cmbTujuan.Location = New Point(182, 199)
+        cmbTujuan.Name = "cmbTujuan"
+        cmbTujuan.Size = New Size(287, 33)
+        cmbTujuan.TabIndex = 6
+        ' 
+        ' lblHarga
+        ' 
+        lblHarga.AutoSize = True
+        lblHarga.Location = New Point(41, 260)
+        lblHarga.Name = "lblHarga"
+        lblHarga.Size = New Size(102, 25)
+        lblHarga.TabIndex = 7
+        lblHarga.Text = "Harga Tiket"
+        ' 
+        ' txtHarga
+        ' 
+        txtHarga.Location = New Point(182, 257)
+        txtHarga.Name = "txtHarga"
+        txtHarga.Size = New Size(287, 31)
+        txtHarga.TabIndex = 8
+        ' 
+        ' lblJumlah
+        ' 
+        lblJumlah.AutoSize = True
+        lblJumlah.Location = New Point(41, 317)
+        lblJumlah.Name = "lblJumlah"
+        lblJumlah.Size = New Size(67, 25)
+        lblJumlah.TabIndex = 9
+        lblJumlah.Text = "Jumlah"
+        ' 
+        ' txtJumlah
+        ' 
+        txtJumlah.Location = New Point(182, 317)
+        txtJumlah.Name = "txtJumlah"
+        txtJumlah.Size = New Size(287, 31)
+        txtJumlah.TabIndex = 10
+        ' 
+        ' lblTotalHarga
+        ' 
+        lblTotalHarga.AutoSize = True
+        lblTotalHarga.Location = New Point(41, 377)
+        lblTotalHarga.Name = "lblTotalHarga"
+        lblTotalHarga.Size = New Size(102, 25)
+        lblTotalHarga.TabIndex = 11
+        lblTotalHarga.Text = "Total Harga"
+        ' 
+        ' txtTotalHarga
+        ' 
+        txtTotalHarga.Location = New Point(182, 377)
+        txtTotalHarga.Name = "txtTotalHarga"
+        txtTotalHarga.Size = New Size(287, 31)
+        txtTotalHarga.TabIndex = 12
+        ' 
+        ' btnPesan
+        ' 
+        btnPesan.Location = New Point(278, 445)
+        btnPesan.Name = "btnPesan"
+        btnPesan.Size = New Size(112, 34)
+        btnPesan.TabIndex = 13
+        btnPesan.Text = "Pesan"
+        btnPesan.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.free_photo_of_jalur_indah_di_pegunungan_trentino_alto_adige
+        ClientSize = New Size(581, 747)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtNoHP As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents lblNoHP As Label
+    Friend WithEvents lblNama As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblHarga As Label
+    Friend WithEvents cmbTujuan As ComboBox
+    Friend WithEvents lblTujuan As Label
+    Friend WithEvents btnPesan As Button
+    Friend WithEvents txtTotalHarga As TextBox
+    Friend WithEvents lblTotalHarga As Label
+    Friend WithEvents txtJumlah As TextBox
+    Friend WithEvents lblJumlah As Label
+    Friend WithEvents txtHarga As TextBox
+
+End Class
---
+++ TugasAkhir-Skha/TugasAkhir-Skha/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    ' Array untuk menyimpan daftar tujuan dan harga tiket
+    Dim tujuan() As String = {"Bali", "Yogyakarta", "Bandung"}
+    Dim hargaTiket() As Integer = {500000, 300000, 200000}
+
+    ' Event handler untuk tombol Pesan
+    Private Sub btnPesan_Click(sender As Object, e As EventArgs) Handles btnPesan.Click
+        ' Mendeklarasikan variabel untuk menyimpan data input
+        Dim nama As String = txtNama.Text
+        Dim noHP As String = txtNoHP.Text
+        Dim tujuanPilihan As String = cmbTujuan.SelectedItem.ToString()
+        Dim harga As Integer = 0
+        Dim jumlah As Integer = Convert.ToInt32(txtJumlah.Text)
+        Dim totalHarga As Integer = 0
+        Dim pesan As String = btnPesan.Text
+
+        ' Menggunakan perulangan untuk mencari harga tiket berdasarkan tujuan
+        For i As Integer = 0 To tujuan.Length - 1
+            If tujuanPilihan = tujuan(i) Then
+                harga = hargaTiket(i)
+                totalHarga = harga * jumlah
+                Exit For
+            End If
+        Next
+
+        ' Menampilkan total harga
+        txtTotalHarga.Text = totalHarga.ToString()
+
+        ' Menampilkan informasi pemesanan melalui MessageBox
+        MessageBox.Show("Pemesanan berhasil dilakukan!" & vbCrLf &
+                        "Nama: " & nama & vbCrLf &
+                        "No HP: " & noHP & vbCrLf &
+                        "Tujuan: " & tujuanPilihan & vbCrLf &
+                        "Jumlah Tiket: " & jumlah & vbCrLf &
+                        "Total Harga: " & totalHarga.ToString() & vbCrLf &
+                        "Pesan: " & pesan)
+    End Sub
+
+    ' Event handler untuk ComboBox Tujuan (untuk menampilkan daftar tujuan)
+    Private Sub cmbTujuan_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbTujuan.SelectedIndexChanged
+        ' Harga tiket akan otomatis diperbarui setelah tujuan dipilih
+        Dim harga As Integer = 0
+        For i As Integer = 0 To tujuan.Length - 1
+            If cmbTujuan.SelectedItem.ToString() = tujuan(i) Then
+                harga = hargaTiket(i)
+                txtHarga.Text = harga.ToString()
+                Exit For
+            End If
+        Next
+    End Sub
+
+    ' Form Load untuk set default pada beberapa kontrol
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Menambahkan pilihan tujuan pada ComboBox menggunakan perulangan
+        For Each item In tujuan
+            cmbTujuan.Items.Add(item)
+        Next
+
+        ' Set default pilihan tujuan
+        cmbTujuan.SelectedIndex = 0  ' Default ke Bali
+    End Sub
+End Class
